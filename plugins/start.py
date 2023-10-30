d7fa3433c0fd112286c4c4ddb369e6d080999278
Brave4reason <126932235+Brave4reason@users.noreply.github.com>
2023-10-30 09:15:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -66,9 +66,8 @@ async def start_command(client: Client, message: Message):
         for msg in messages:
 
             if bool(CUSTOM_CAPTION) & bool(msg.document):
-            l = await caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
-            await asyncio.sleep(10) 
-            await l.delete() 
+            await caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)       
+            
             else:
                 caption = "" if not msg.caption else msg.caption.html
 
@@ -83,6 +82,7 @@ async def start_command(client: Client, message: Message):
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                await asyncio.sleep(0.5)
             except:
                 pass
         return
