0420f6ae96bf2515d20c131fe26a4a768501901b
Brave4reason <126932235+Brave4reason@users.noreply.github.com>
2023-10-30 14:27:54 +0530

    Update and rename start1.py to start.py

--- plugins/start.py
+++ plugins/start.py
@@ -62,7 +62,7 @@ async def start_command(client: Client, message: Message):
             await message.reply_text("Something went wrong..!")
             return
         await temp_msg.delete()
-        await asyncio.sleep(300)
+        await asyncio.sleep(10)
         await messages.delete()
 
         for msg in messages:
